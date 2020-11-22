b9ab6054e7c89ab902bedbbff8285c7c978b0782
Erickowski <daniel_perez_03081995@hotmail.com>
2020-11-22 17:53:42 -0600

    Instalando tailwind y normalize

--- componentes/Layout.jsx
+++ componentes/Layout.jsx
@@ -1,9 +1,25 @@
 import React from "react";
+import Head from "next/head";
+
+import Sidebar from "./Sidebar";
 
 const Layout = ({ children }) => {
   return (
     <>
-      <h1>Desde Layout</h1>
+      <Head>
+        <title>CRM - Administración de clientes</title>
+        <link
+          rel="stylesheet"
+          href="https://cdnjs.cloudflare.com/ajax/libs/normalize/8.0.1/normalize.css"
+          integrity="sha512-oHDEc8Xed4hiW6CxD7qjbnI+B07vDdX7hEPTvn9pSZO1bcRqHp8mj9pyr+8RVC2GmtEfI2Bi9Ke9Ass0as+zpg=="
+          crossorigin="anonymous"
+        />
+        <link
+          href="https://unpkg.com/tailwindcss@^2/dist/tailwind.min.css"
+          rel="stylesheet"
+        />
+      </Head>
+      <Sidebar />
       {children}
     </>
   );
