b104ba695dee92881ebd0b2f2e38e4f7df4bc683
Erickowski <daniel_perez_03081995@hotmail.com>
2020-11-22 18:49:49 -0600

    Mostrar pagina actual

--- componentes/Sidebar.jsx
+++ componentes/Sidebar.jsx
@@ -1,21 +1,32 @@
 import React from "react";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 const Sidebar = () => {
+  const router = useRouter();
   return (
     <aside className="bg-gray-800 sm:w-1/3 xl:w-1/5 sm:min-h-screen p-5">
       <div>
         <p className="text-white text-2xl font-black">CRM Clientes</p>
       </div>
       <nav className="mt-5 list-none">
-        <li>
+        <li className={`p-2 ${router.pathname === "/" && "bg-blue-800"}`}>
           <Link href="/">
-            <a className="text-white mb-2 block">Clientes</a>
+            <a className="text-white block">Clientes</a>
           </Link>
         </li>
-        <li>
+        <li
+          className={`p-2 ${router.pathname === "/pedidos" && "bg-blue-800"}`}
+        >
           <Link href="/pedidos">
-            <a className="text-white mb-2 block">Pedidos</a>
+            <a className="text-white block">Pedidos</a>
+          </Link>
+        </li>
+        <li
+          className={`p-2 ${router.pathname === "/productos" && "bg-blue-800"}`}
+        >
+          <Link href="/productos">
+            <a className="text-white block">Productos</a>
           </Link>
         </li>
       </nav>
