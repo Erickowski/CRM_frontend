cb736c309880b3417856bccf2bee7c24a09742c9
Erickowski <daniel_perez_03081995@hotmail.com>
2020-11-22 18:55:30 -0600

    Paginas de login y nueva cuenta

---
+++ pages/nueva-cuenta.jsx
@@ -0,0 +1,9 @@
+import Layout from "../componentes/Layout";
+
+export default function NuevaCuenta() {
+  return (
+    <Layout>
+      <h1 className="text-2xl text-gray-800 font-light">NuevaCuenta</h1>
+    </Layout>
+  );
+}
