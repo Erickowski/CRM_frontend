820cd1db434b676167e8087dbd915220d3731134
Erickowski <daniel_perez_03081995@hotmail.com>
2020-11-28 16:49:18 -0600

    Finalizando validacion

--- pages/nueva-cuenta.jsx
+++ pages/nueva-cuenta.jsx
@@ -13,6 +13,13 @@ export default function NuevaCuenta() {
     },
     validationSchema: Yup.object({
       nombre: Yup.string().required("El nombre es obligatorio."),
+      apellido: Yup.string().required("El apellido es obligatorio."),
+      email: Yup.string()
+        .email("El email no es válido.")
+        .required("El email es obligatorio."),
+      password: Yup.string()
+        .required("El password no puede ir vacío.")
+        .min(6, "El password debe ser de al menos 6 caracteres."),
     }),
     onSubmit: (valores) => {
       console.log("enviando");
@@ -68,8 +75,15 @@ export default function NuevaCuenta() {
                 placeholder="Apellido usuario"
                 value={formik.values.apellido}
                 onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
               />
             </div>
+            {formik.touched.apellido && formik.errors.apellido && (
+              <div className="my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-4">
+                <p className="font-bold">Error</p>
+                <p>{formik.errors.apellido}</p>
+              </div>
+            )}
             <div className="mb-4">
               <label
                 className="block text-gray-700 text-sm font-bold mb-2"
@@ -84,8 +98,15 @@ export default function NuevaCuenta() {
                 placeholder="Email usuario"
                 value={formik.values.email}
                 onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
               />
             </div>
+            {formik.touched.email && formik.errors.email && (
+              <div className="my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-4">
+                <p className="font-bold">Error</p>
+                <p>{formik.errors.email}</p>
+              </div>
+            )}
             <div className="mb-4">
               <label
                 className="block text-gray-700 text-sm font-bold mb-2"
@@ -100,8 +121,15 @@ export default function NuevaCuenta() {
                 placeholder="Password usuario"
                 value={formik.values.password}
                 onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
               />
             </div>
+            {formik.touched.password && formik.errors.password && (
+              <div className="my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-4">
+                <p className="font-bold">Error</p>
+                <p>{formik.errors.password}</p>
+              </div>
+            )}
             <input
               type="submit"
               value="Crear cuenta"
