769a4f2b88024825cc70f12221e91a317e94babb
Erickowski <daniel_perez_03081995@hotmail.com>
2020-11-28 16:32:12 -0600

    Agregando validacion

--- pages/nueva-cuenta.jsx
+++ pages/nueva-cuenta.jsx
@@ -11,6 +11,9 @@ export default function NuevaCuenta() {
       email: "",
       password: "",
     },
+    validationSchema: Yup.object({
+      nombre: Yup.string().required("El nombre es obligatorio."),
+    }),
     onSubmit: (valores) => {
       console.log("enviando");
       console.log(valores);
@@ -42,8 +45,15 @@ export default function NuevaCuenta() {
                 placeholder="Nombre usuario"
                 value={formik.values.nombre}
                 onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
               />
             </div>
+            {formik.touched.nombre && formik.errors.nombre && (
+              <div className="my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-4">
+                <p className="font-bold">Error</p>
+                <p>{formik.errors.nombre}</p>
+              </div>
+            )}
             <div className="mb-4">
               <label
                 className="block text-gray-700 text-sm font-bold mb-2"
