c0f97f467f1e51c0536c866735f8231b8f8bb83c
Erickowski <daniel_perez_03081995@hotmail.com>
2020-11-28 17:58:10 -0600

    Validacion en login

--- pages/login.jsx
+++ pages/login.jsx
@@ -1,12 +1,33 @@
 import Layout from "../componentes/Layout";
+import { useFormik } from "formik";
+import * as Yup from "yup";
 
 export default function Login() {
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    validationSchema: Yup.object({
+      email: Yup.string()
+        .email("El email no es válido.")
+        .required("El email no puede ir vacio."),
+      password: Yup.string().required("El password es obligatorio."),
+    }),
+    onSubmit: (valores) => {
+      console.log(valores);
+    },
+  });
+
   return (
     <Layout>
       <h1 className="text-center text-2xl text-white font-light">Login</h1>
       <div className="flex justify-center mt-5">
         <div className="w-full max-w-sm">
-          <form className="bg-white rounded shadow-md px-8 pt-6 pb-8 mb-4">
+          <form
+            className="bg-white rounded shadow-md px-8 pt-6 pb-8 mb-4"
+            onSubmit={formik.handleSubmit}
+          >
             <div className="mb-4">
               <label
                 htmlFor=""
@@ -20,8 +41,17 @@ export default function Login() {
                 type="email"
                 className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus: shadow-outline"
                 placeholder="Email usuario"
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                value={formik.values.email}
               />
             </div>
+            {formik.touched.email && formik.errors.email && (
+              <div className="my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-4">
+                <p className="font-bold">Error</p>
+                <p>{formik.errors.email}</p>
+              </div>
+            )}
             <div className="mb-4">
               <label
                 htmlFor=""
@@ -35,8 +65,17 @@ export default function Login() {
                 type="password"
                 className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus: shadow-outline"
                 placeholder="Password usuario"
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
+                value={formik.values.password}
               />
             </div>
+            {formik.touched.password && formik.errors.password && (
+              <div className="my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-4">
+                <p className="font-bold">Error</p>
+                <p>{formik.errors.password}</p>
+              </div>
+            )}
             <input
               type="submit"
               value="Iniciar sesión"
