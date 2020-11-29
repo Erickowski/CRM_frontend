b27018425ebedf978768fc3d527400d51f65422e
Erickowski <daniel_perez_03081995@hotmail.com>
2020-11-29 13:06:55 -0600

    Formulario para crear nuevo producto

--- pages/nuevo-producto.jsx
+++ pages/nuevo-producto.jsx
@@ -4,6 +4,71 @@ export default function NuevoProducto() {
   return (
     <Layout>
       <h1 className="text-2xl text-gray-800 font-light">Nuevo Producto</h1>
+      <div className="flex justify-center mt-5">
+        <div className="w-full max-w-lg">
+          <form
+            className="bg-white shadow-md px-8 pt-6 pb-8 mb-4"
+            // onSubmit={formik.handleSubmit}
+          >
+            <div className="mb-4">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="nombre"
+              >
+                Nombre
+              </label>
+              <input
+                id="nombre"
+                type="text"
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus: shadow-outline"
+                placeholder="Nombre producto"
+                // value={formik.values.nombre}
+                // onChange={formik.handleChange}
+                // onBlur={formik.handleBlur}
+              />
+            </div>
+            <div className="mb-4">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="existencia"
+              >
+                Cantidad disponible
+              </label>
+              <input
+                id="existencia"
+                type="number"
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus: shadow-outline"
+                placeholder="Cantidad disponible"
+                // value={formik.values.existencia}
+                // onChange={formik.handleChange}
+                // onBlur={formik.handleBlur}
+              />
+            </div>
+            <div className="mb-4">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="precio"
+              >
+                Precio
+              </label>
+              <input
+                id="precio"
+                type="number"
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus: shadow-outline"
+                placeholder="Precio Producto"
+                // value={formik.values.precio}
+                // onChange={formik.handleChange}
+                // onBlur={formik.handleBlur}
+              />
+            </div>
+            <input
+              type="submit"
+              value="Agregar nuevo producto"
+              className="bg-gray-800 w-full mt-5 p-2 text-white uppercase font-bold hover:bg-gray-900 cursor-pointer"
+            />
+          </form>
+        </div>
+      </div>
     </Layout>
   );
 }
