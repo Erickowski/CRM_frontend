2ac535e24a000bbaafa2e173251985e300e7ab0b
Erickowski <daniel_perez_03081995@hotmail.com>
2020-11-29 12:38:38 -0600

    Tabla para productos

--- pages/productos.jsx
+++ pages/productos.jsx
@@ -1,9 +1,44 @@
+import { gql, useQuery } from "@apollo/client";
+
 import Layout from "../componentes/Layout";
+import Producto from "../componentes/Producto";
+
+const OBTENER_PRODUCTOS = gql`
+  query obtenerProductos {
+    obtenerProductos {
+      id
+      nombre
+      precio
+      existencia
+    }
+  }
+`;
 
 export default function Productos() {
+  // Consultar los productos
+  const { data, loading, error } = useQuery(OBTENER_PRODUCTOS);
+
+  if (loading) return "Cargando...";
+
   return (
     <Layout>
       <h1 className="text-2xl text-gray-800 font-light">Productos</h1>
+      <table className="table-auto shadow-md mt-10 w-full w-lg">
+        <thead className="bg-gray-800">
+          <tr className="text-white">
+            <th className="w-1/5 py-2">Nombre</th>
+            <th className="w-1/5 py-2">Existencia</th>
+            <th className="w-1/5 py-2">Precio</th>
+            <th className="w-1/5 py-2">Eliminar</th>
+            <th className="w-1/5 py-2">Editar</th>
+          </tr>
+        </thead>
+        <tbody className="bg-white">
+          {data.obtenerProductos.map((producto) => (
+            <Producto key={producto.id} producto={producto} />
+          ))}
+        </tbody>
+      </table>
     </Layout>
   );
 }
