d2ae6442dfb244a3358a954ac1aeaf07754b1b07
Erickowski <daniel_perez_03081995@hotmail.com>
2020-11-29 14:28:06 -0600

    Pagina para crear pedido

---
+++ pages/nuevo-pedido.jsx
@@ -0,0 +1,9 @@
+import Layout from "../componentes/Layout";
+
+export default function NuevoPedido() {
+  return (
+    <Layout>
+      <h1 className="text-2xl text-gray-800 font-light">Nuevo Pedido</h1>
+    </Layout>
+  );
+}
--- pages/pedidos.jsx
+++ pages/pedidos.jsx
@@ -1,9 +1,16 @@
+import Link from "next/link";
+
 import Layout from "../componentes/Layout";
 
 export default function Pedidos() {
   return (
     <Layout>
       <h1 className="text-2xl text-gray-800 font-light">Pedidos</h1>
+      <Link href="/nuevo-pedido">
+        <a className="bg-blue-800 py-2 px-5 mt-5 inline-block text-white rounded text-sm hover:bg-gray-800 mb-3 uppercase font-bold">
+          Nuevo Pedido
+        </a>
+      </Link>
     </Layout>
   );
 }
