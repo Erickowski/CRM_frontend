f6bba256ad5f35dfa4ed7b9cd347459669770c73
Erickowski <daniel_perez_03081995@hotmail.com>
2020-11-28 19:52:04 -0600

    Layout para crear nuevo cliente

--- pages/nuevo-cliente.jsx
+++ pages/nuevo-cliente.jsx
@@ -4,6 +4,102 @@ export default function NuevoCliente() {
   return (
     <Layout>
       <h1 className="text-2xl text-gray-800 font-light">Nuevo Cliente</h1>
+      <div className="flex justify-center mt-5">
+        <div className="w-full max-w-lg">
+          <form className="bg-white shadow-md px-8 pt-6 pb-8 mb-4">
+            <div className="mb-4">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="nombre"
+              >
+                Nombre
+              </label>
+              <input
+                id="nombre"
+                type="text"
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus: shadow-outline"
+                placeholder="Nombre cliente"
+                // value={formik.values.nombre}
+                // onChange={formik.handleChange}
+                // onBlur={formik.handleBlur}
+              />
+            </div>
+            <div className="mb-4">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="apellido"
+              >
+                Apellido
+              </label>
+              <input
+                id="apellido"
+                type="text"
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus: shadow-outline"
+                placeholder="Apellido cliente"
+                // value={formik.values.apellido}
+                // onChange={formik.handleChange}
+                // onBlur={formik.handleBlur}
+              />
+            </div>
+            <div className="mb-4">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="empresa"
+              >
+                Empresa
+              </label>
+              <input
+                id="empresa"
+                type="text"
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus: shadow-outline"
+                placeholder="Empresa cliente"
+                // value={formik.values.empresa}
+                // onChange={formik.handleChange}
+                // onBlur={formik.handleBlur}
+              />
+            </div>
+            <div className="mb-4">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="email"
+              >
+                Email
+              </label>
+              <input
+                id="email"
+                type="email"
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus: shadow-outline"
+                placeholder="Email cliente"
+                // value={formik.values.email}
+                // onChange={formik.handleChange}
+                // onBlur={formik.handleBlur}
+              />
+            </div>
+            <div className="mb-4">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="telefono"
+              >
+                Telefono
+              </label>
+              <input
+                id="telefono"
+                type="tel"
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus: shadow-outline"
+                placeholder="Telefono cliente"
+                // value={formik.values.telefono}
+                // onChange={formik.handleChange}
+                // onBlur={formik.handleBlur}
+              />
+            </div>
+            <input
+              type="submit"
+              value="Registrar cliente"
+              className="bg-gray-800 w-full mt-5 p-2 text-white uppercase font-bold hover:bg-gray-900 cursor-pointer"
+            />
+          </form>
+        </div>
+      </div>
     </Layout>
   );
 }
