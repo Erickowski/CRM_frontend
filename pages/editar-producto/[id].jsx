a93b2c3651c97d6b7e4ab98a792bc5078f160d38
Erickowski <daniel_perez_03081995@hotmail.com>
2020-11-29 13:47:50 -0600

    Formulario para editar producto

--- pages/editar-producto/[id].jsx
+++ pages/editar-producto/[id].jsx
@@ -11,6 +11,89 @@ export default function EditarProducto() {
   return (
     <Layout>
       <h1 className="text-2xl text-gray-800 font-light">Editar Producto</h1>
+      <div className="flex justify-center mt-5">
+        <div className="w-full max-w-lg">
+          <form
+            className="bg-white shadow-md px-8 pt-6 pb-8 mb-4"
+            // onSubmit={formik.handleSubmit}
+          >
+            <div className="mb-4">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="nombre"
+              >
+                Nombre
+              </label>
+              <input
+                id="nombre"
+                type="text"
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus: shadow-outline"
+                placeholder="Nombre producto"
+                // value={formik.values.nombre}
+                // onChange={formik.handleChange}
+                // onBlur={formik.handleBlur}
+              />
+            </div>
+            {/* {formik.touched.nombre && formik.errors.nombre && (
+              <div className="my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-4">
+                <p className="font-bold">Error</p>
+                <p>{formik.errors.nombre}</p>
+              </div>
+            )} */}
+            <div className="mb-4">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="existencia"
+              >
+                Cantidad disponible
+              </label>
+              <input
+                id="existencia"
+                type="number"
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus: shadow-outline"
+                placeholder="Cantidad disponible"
+                // value={formik.values.existencia}
+                // onChange={formik.handleChange}
+                // onBlur={formik.handleBlur}
+              />
+            </div>
+            {/* {formik.touched.existencia && formik.errors.existencia && (
+              <div className="my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-4">
+                <p className="font-bold">Error</p>
+                <p>{formik.errors.existencia}</p>
+              </div>
+            )} */}
+            <div className="mb-4">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="precio"
+              >
+                Precio
+              </label>
+              <input
+                id="precio"
+                type="number"
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus: shadow-outline"
+                placeholder="Precio Producto"
+                // value={formik.values.precio}
+                // onChange={formik.handleChange}
+                // onBlur={formik.handleBlur}
+              />
+            </div>
+            {/* {formik.touched.precio && formik.errors.precio && (
+              <div className="my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-4">
+                <p className="font-bold">Error</p>
+                <p>{formik.errors.precio}</p>
+              </div>
+            )} */}
+            <input
+              type="submit"
+              value="Editar producto"
+              className="bg-gray-800 w-full mt-5 p-2 text-white uppercase font-bold hover:bg-gray-900 cursor-pointer"
+            />
+          </form>
+        </div>
+      </div>
     </Layout>
   );
 }
