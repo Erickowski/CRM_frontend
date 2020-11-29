6b18ca56095e852fe06798bd2be8eada43565466
Erickowski <daniel_perez_03081995@hotmail.com>
2020-11-29 14:56:03 -0600

    Iniciando con el context

---
+++ context/pedidos/PedidosReducer.jsx
@@ -0,0 +1,14 @@
+import {
+  CANTIDAD_PRODUCTOS,
+  SELECCIONAR_CLIENTE,
+  SELECCIONAR_PRODUCTO,
+} from "../../types";
+
+const PedidoReducer = (state, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default PedidoReducer;
---
+++ context/pedidos/PedidosState.jsx
@@ -0,0 +1,10 @@
+import React, { useReducer } from "react";
+
+import PedidoContext from "./PedidosContext";
+import PedidoReducer from "./PedidosReducer";
+
+import {
+  CANTIDAD_PRODUCTOS,
+  SELECCIONAR_CLIENTE,
+  SELECCIONAR_PRODUCTO,
+} from "../../types";
