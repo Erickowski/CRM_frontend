379017576dec8f1634a5e4d5fee1dc20fd776cf7
Erickowski <daniel_perez_03081995@hotmail.com>
2020-11-28 18:57:43 -0600

    Mostrar clientes del vendedor autenticado

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,45 @@
 import Layout from "../componentes/Layout";
+import { gql, useQuery } from "@apollo/client";
+
+const OBTENER_CLIENTES_USUARIO = gql`
+  query obtenerClientesVendedor {
+    obtenerClientesVendedor {
+      nombre
+      apellido
+      empresa
+      email
+    }
+  }
+`;
 
 export default function Home() {
+  // Consulta de Apollo
+  const { data, loading, error } = useQuery(OBTENER_CLIENTES_USUARIO);
+
+  if (loading) return "Cargando...";
   return (
     <Layout>
       <h1 className="text-2xl text-gray-800 font-light">Clientes</h1>
+      <table className="table-auto shadow-md mt-10 w-full w-lg">
+        <thead className="bg-gray-800">
+          <tr className="text-white">
+            <th className="w-1/5 py-2">Nombre</th>
+            <th className="w-1/5 py-2">Empresa</th>
+            <th className="w-1/5 py-2">Email</th>
+          </tr>
+        </thead>
+        <tbody className="bg-white">
+          {data.obtenerClientesVendedor.map((cliente) => (
+            <tr key={cliente.id}>
+              <td className="border px-2 py-2">
+                {cliente.nombre} {cliente.apellido}
+              </td>
+              <td className="border px-2 py-2">{cliente.empresa}</td>
+              <td className="border px-2 py-2">{cliente.email}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </Layout>
   );
 }
