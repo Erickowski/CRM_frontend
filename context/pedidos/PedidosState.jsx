ab6ab14532939b82d47871d7d8078e791afb6c76
Erickowski <daniel_perez_03081995@hotmail.com>
2020-11-29 15:06:04 -0600

    Añadiendo el context de pedidos a la aplicación

--- context/pedidos/PedidosState.jsx
+++ context/pedidos/PedidosState.jsx
@@ -8,3 +8,18 @@ import {
   SELECCIONAR_CLIENTE,
   SELECCIONAR_PRODUCTO,
 } from "../../types";
+
+const PedidoState = ({ children }) => {
+  // State de pedidos
+  const initialState = {
+    cliente: [],
+    productos: [],
+    total: 0,
+  };
+
+  const [state, dispatch] = useReducer(PedidoReducer, initialState);
+
+  return <PedidoContext.Provider value={{}}>{children}</PedidoContext.Provider>;
+};
+
+export default PedidoState;
