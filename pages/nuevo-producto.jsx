0adcaa9a8bfe54ccf622bdb85dbe726218de37f0
Erickowski <daniel_perez_03081995@hotmail.com>
2020-11-29 13:13:56 -0600

    Agregando validacion a formulario de nuevo producto

--- pages/nuevo-producto.jsx
+++ pages/nuevo-producto.jsx
@@ -1,6 +1,31 @@
+import { useFormik } from "formik";
+import * as Yup from "yup";
+
 import Layout from "../componentes/Layout";
 
 export default function NuevoProducto() {
+  // Formulario para nuevos productos
+  const formik = useFormik({
+    initialValues: {
+      nombre: "",
+      existencia: "",
+      precio: "",
+    },
+    validationSchema: Yup.object({
+      nombre: Yup.string().required("El nombre del producto es obligatorio."),
+      existencia: Yup.number()
+        .required("Agrega la cantidad disponible.")
+        .positive("No se aceptan números negativos.")
+        .integer("La existencia deben ser números enteros."),
+      precio: Yup.number()
+        .required("Agrega la cantidad disponible.")
+        .positive("No se aceptan números negativos."),
+    }),
+    onSubmit: () => {
+      console.log("submit");
+    },
+  });
+
   return (
     <Layout>
       <h1 className="text-2xl text-gray-800 font-light">Nuevo Producto</h1>
@@ -8,7 +33,7 @@ export default function NuevoProducto() {
         <div className="w-full max-w-lg">
           <form
             className="bg-white shadow-md px-8 pt-6 pb-8 mb-4"
-            // onSubmit={formik.handleSubmit}
+            onSubmit={formik.handleSubmit}
           >
             <div className="mb-4">
               <label
@@ -22,11 +47,17 @@ export default function NuevoProducto() {
                 type="text"
                 className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus: shadow-outline"
                 placeholder="Nombre producto"
-                // value={formik.values.nombre}
-                // onChange={formik.handleChange}
-                // onBlur={formik.handleBlur}
+                value={formik.values.nombre}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
               />
             </div>
+            {formik.touched.nombre && formik.errors.nombre && (
+              <div className="my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-4">
+                <p className="font-bold">Error</p>
+                <p>{formik.errors.nombre}</p>
+              </div>
+            )}
             <div className="mb-4">
               <label
                 className="block text-gray-700 text-sm font-bold mb-2"
@@ -39,11 +70,17 @@ export default function NuevoProducto() {
                 type="number"
                 className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus: shadow-outline"
                 placeholder="Cantidad disponible"
-                // value={formik.values.existencia}
-                // onChange={formik.handleChange}
-                // onBlur={formik.handleBlur}
+                value={formik.values.existencia}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
               />
             </div>
+            {formik.touched.existencia && formik.errors.existencia && (
+              <div className="my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-4">
+                <p className="font-bold">Error</p>
+                <p>{formik.errors.existencia}</p>
+              </div>
+            )}
             <div className="mb-4">
               <label
                 className="block text-gray-700 text-sm font-bold mb-2"
@@ -56,11 +93,17 @@ export default function NuevoProducto() {
                 type="number"
                 className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus: shadow-outline"
                 placeholder="Precio Producto"
-                // value={formik.values.precio}
-                // onChange={formik.handleChange}
-                // onBlur={formik.handleBlur}
+                value={formik.values.precio}
+                onChange={formik.handleChange}
+                onBlur={formik.handleBlur}
               />
             </div>
+            {formik.touched.precio && formik.errors.precio && (
+              <div className="my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-4">
+                <p className="font-bold">Error</p>
+                <p>{formik.errors.precio}</p>
+              </div>
+            )}
             <input
               type="submit"
               value="Agregar nuevo producto"
