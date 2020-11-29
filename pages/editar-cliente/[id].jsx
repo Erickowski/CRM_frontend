5dd7bee9eb9accf5a62544fe1e4aff6f48f61731
Erickowski <daniel_perez_03081995@hotmail.com>
2020-11-29 11:24:50 -0600

    Layout para editar cliente

--- pages/editar-cliente/[id].jsx
+++ pages/editar-cliente/[id].jsx
@@ -1,5 +1,7 @@
 import { useRouter } from "next/router";
 
+import Layout from "../../componentes/Layout";
+
 export default function EditarCliente() {
   // Obtener el ID actual
   const router = useRouter();
@@ -7,5 +9,132 @@ export default function EditarCliente() {
     query: { id },
   } = router;
   console.log(id);
-  return <h1>Desde editar cliente</h1>;
+  return (
+    <Layout>
+      <h1 className="text-2xl text-gray-800 font-light">Editar Cliente</h1>
+      <div className="flex justify-center mt-5">
+        <div className="w-full max-w-lg">
+          <form
+            className="bg-white shadow-md px-8 pt-6 pb-8 mb-4"
+            // onSubmit={formik.handleSubmit}
+          >
+            <div className="mb-4">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="nombre"
+              >
+                Nombre
+              </label>
+              <input
+                id="nombre"
+                type="text"
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus: shadow-outline"
+                placeholder="Nombre cliente"
+                // value={formik.values.nombre}
+                // onChange={formik.handleChange}
+                // onBlur={formik.handleBlur}
+              />
+            </div>
+            {/* {formik.touched.nombre && formik.errors.nombre && (
+              <div className="my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-4">
+                <p className="font-bold">Error</p>
+                <p>{formik.errors.nombre}</p>
+              </div>
+            )} */}
+            <div className="mb-4">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="apellido"
+              >
+                Apellido
+              </label>
+              <input
+                id="apellido"
+                type="text"
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus: shadow-outline"
+                placeholder="Apellido cliente"
+                // value={formik.values.apellido}
+                // onChange={formik.handleChange}
+                // onBlur={formik.handleBlur}
+              />
+            </div>
+            {/* {formik.touched.apellido && formik.errors.apellido && (
+              <div className="my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-4">
+                <p className="font-bold">Error</p>
+                <p>{formik.errors.apellido}</p>
+              </div>
+            )} */}
+            <div className="mb-4">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="empresa"
+              >
+                Empresa
+              </label>
+              <input
+                id="empresa"
+                type="text"
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus: shadow-outline"
+                placeholder="Empresa cliente"
+                // value={formik.values.empresa}
+                // onChange={formik.handleChange}
+                // onBlur={formik.handleBlur}
+              />
+            </div>
+            {/* {formik.touched.empresa && formik.errors.empresa && (
+              <div className="my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-4">
+                <p className="font-bold">Error</p>
+                <p>{formik.errors.empresa}</p>
+              </div>
+            )} */}
+            <div className="mb-4">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="email"
+              >
+                Email
+              </label>
+              <input
+                id="email"
+                type="email"
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus: shadow-outline"
+                placeholder="Email cliente"
+                // value={formik.values.email}
+                // onChange={formik.handleChange}
+                // onBlur={formik.handleBlur}
+              />
+            </div>
+            {/* {formik.touched.email && formik.errors.email && (
+              <div className="my-2 bg-red-100 border-l-4 border-red-500 text-red-700 p-4">
+                <p className="font-bold">Error</p>
+                <p>{formik.errors.email}</p>
+              </div>
+            )} */}
+            <div className="mb-4">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                htmlFor="telefono"
+              >
+                Telefono
+              </label>
+              <input
+                id="telefono"
+                type="tel"
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus: shadow-outline"
+                placeholder="Telefono cliente"
+                // value={formik.values.telefono}
+                // onChange={formik.handleChange}
+                // onBlur={formik.handleBlur}
+              />
+            </div>
+            <input
+              type="submit"
+              value="Editar cliente"
+              className="bg-gray-800 w-full mt-5 p-2 text-white uppercase font-bold hover:bg-gray-900 cursor-pointer"
+            />
+          </form>
+        </div>
+      </div>
+    </Layout>
+  );
 }
