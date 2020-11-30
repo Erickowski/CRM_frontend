05753b19e5de1d91971ef5a5baf860050c5bdc7f
Erickowski <daniel_perez_03081995@hotmail.com>
2020-11-29 18:20:44 -0600

    Insertar pedidos en la base de datos

--- pages/nuevo-pedido.jsx
+++ pages/nuevo-pedido.jsx
@@ -1,4 +1,7 @@
-import { useContext } from "react";
+import { useContext, useState } from "react";
+import { gql, useMutation } from "@apollo/client";
+import { useRouter } from "next/router";
+import Swal from "sweetalert2";
 
 import Layout from "../componentes/Layout";
 
@@ -9,8 +12,23 @@ import Total from "../componentes/pedidos/Total";
 
 import PedidoContext from "../context/pedidos/PedidosContext";
 
+const NUEVO_PEDIDO = gql`
+  mutation nuevoPedido($input: PedidoInput) {
+    nuevoPedido(input: $input) {
+      id
+    }
+  }
+`;
+
 export default function NuevoPedido() {
+  const router = useRouter();
+
+  const [mensaje, setMensaje] = useState(null);
+
   const { cliente, productos, total } = useContext(PedidoContext);
+
+  // Mutation para crear nuevo pedido
+  const [nuevoPedido] = useMutation(NUEVO_PEDIDO);
 
   const validarPedido = () => {
     return !productos.every((producto) => producto.cantidad > 0) ||
@@ -20,9 +38,51 @@ export default function NuevoPedido() {
       : "";
   };
 
+  const crearNuevoPedido = async () => {
+    // Remover lo no deseado de productos
+    const pedido = productos.map(
+      ({ existencia, __typename, ...producto }) => producto
+    );
+
+    try {
+      const { id } = cliente;
+      await nuevoPedido({
+        variables: {
+          input: {
+            cliente: id,
+            total,
+            pedido,
+          },
+        },
+      });
+      // Redireccionar
+      router.push("/pedidos");
+      // Mostrar alerta
+      Swal.fire(
+        "Pedido creado.",
+        "El pedido fue creado correctamente.",
+        "success"
+      );
+    } catch (error) {
+      setMensaje(error.message.replace("GraphQL error: ", ""));
+      setTimeout(() => {
+        setMensaje(null);
+      }, 3000);
+    }
+  };
+
+  const mostrarMensaje = () => {
+    return (
+      <div className="bg-white py-2 px-3 w-full my-3 max-w-sm text-center mx-auto">
+        <p>{mensaje}</p>
+      </div>
+    );
+  };
+
   return (
     <Layout>
       <h1 className="text-2xl text-gray-800 font-light">Nuevo Pedido</h1>
+      {mensaje && mostrarMensaje()}
       <div className="flex justify-center mt-5">
         <div className="w-full max-w-lg">
           <AsignarCliente />
@@ -32,6 +92,7 @@ export default function NuevoPedido() {
           <button
             type="button"
             className={`bg-gray-800 w-full mt-5 p-2 text-white uppercase font-bold hover:bg-gray-900 ${validarPedido()}`}
+            onClick={() => crearNuevoPedido()}
           >
             Registrar pedido
           </button>
